943633e9d5368cd625278cd7d81004d10f28a80f
Kendra Anderson <kor04003@byui.edu>
2022-01-05 16:15:03 -0800

    done

---
+++ tic.py
@@ -0,0 +1,40 @@
+#Assignment 'Tic Tac Toe'
+#Authors: Kendra Anderson
+#1-5-2022
+
+#create variables
+x = 'X'
+o = 'O'
+xColor = '#03fcd7'
+oColor = '#0390fc'
+winnerColor = '#fc1c03'
+
+#create board list
+board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+def main():
+    pass
+    #call print board
+    #print instructions
+    #call get input
+    #call change board index value
+    #call print board
+    #call check for end of game
+
+#print board
+def printBoard():
+    print("\n")
+    print("stop")
+    #print("{} | {} | {}".format(board[0], board[1], board[2]))
+
+#get input
+def getInput():
+    pass
+
+#change board index value
+def changeBoard():
+    pass
+
+#check for end of game
+def endGame():
+    pass
